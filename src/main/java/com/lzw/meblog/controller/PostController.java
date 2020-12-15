295a901a956773649769efca0160a23e9502a6fd
TongxinWong <jue0325@gmail.com>
2020-12-16 01:22:06 +0800

    初步使用Swagger2 显示RESTful api

--- src/main/java/com/lzw/meblog/controller/PostController.java
+++ src/main/java/com/lzw/meblog/controller/PostController.java
@@ -2,11 +2,10 @@ package com.lzw.meblog.controller;
 
 import com.lzw.meblog.model.Post;
 import com.lzw.meblog.service.PostService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,14 +16,17 @@ public class PostController {
     private PostService postService;
 
     @CrossOrigin("http://localhost:8080")
-    @RequestMapping("/getPost/{id}")
-    public Post getPost(@PathVariable int id){
-        return postService.getPostById(id);
+    @ApiOperation(value = "获取文章列表")
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Post> getAllPosts(){
+        return postService.getAllPosts();
     }
 
     @CrossOrigin("http://localhost:8080")
-    @RequestMapping("/getPost/all")
-    public List<Post> getAllPosts(){
-        return postService.getAllPosts();
+    @ApiOperation(value = "获取文章详细信息", notes = "根据id获取文章详细信息")
+    @ApiImplicitParam(name = "id", value = "文章id", required = true, dataType = "int")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Post getPost(@PathVariable int id){
+        return postService.getPostById(id);
     }
 }
